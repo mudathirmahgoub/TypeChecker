8be88d0e4ed419af886d2d3e59b9bb74e045c6d6
mudathirmahgoub <mudathirmahgoub@gmail.com>
2018-04-23 11:59:25 -0500

    common cli

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,32 @@
+import org.apache.commons.cli.*;
+
 public class Main
 {
 
     public static void main(String[] args)
     {
-        System.out.println("TypeChecker");
+        Options options = new Options();
+
+        options.addOption(Option.builder("i").longOpt("input").hasArg().required().build());
+        options.addOption(Option.builder("o").longOpt("output").hasArg().build());
+
+        CommandLineParser commandLineParser = new DefaultParser();
+        try {
+            CommandLine command = commandLineParser.parse(options, args);
+            String inputFile  = command.getOptionValue("i");
+            if (command.hasOption("i"))
+            {
+
+            }
+            else
+            {
+                throw new ParseException("");
+            }
+        }
+        catch (ParseException exception)
+        {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp( "java -jar TypeChecker.jar ", options );
+        }
     }
 }
